8a542994d73387c14b4c405e1419db29fc08c493
Roman Berger <romanberger001@gmail.com>
2022-12-14 20:22:44 +0000

    error messages when inputting

--- src/SensorDataEntry.cpp
+++ src/SensorDataEntry.cpp
@@ -20,9 +20,11 @@ SensDataEntry::SensDataEntry(const SensDataEntry& other){
 // Setters
 bool SensDataEntry::checkAndSetTemporalInfo(int Tval1, int Tval2, int Tval3){
 
-    if(this->checkTemporalInfo(Tval1, Tval2, Tval3)){
+    if(this->checkTemporalInfo(Tval1, Tval2, Tval3) && !temporal_init){
         this->temporal_info = {Tval1, Tval2, Tval3};
         return true;
+    } if(temporal_init){
+        std::cout << "temporal info already set, must reset first" << std::endl;
     }
     return false;
 
@@ -31,9 +33,11 @@ bool SensDataEntry::checkAndSetTemporalInfo(int Tval1, int Tval2, int Tval3){
 bool SensDataEntry::checkTemporalInfo(int Tval1, int Tval2, int Tval3){
 
     if(Tval1 < 2021 || Tval1 > 2023){
+        std::cout << "year invalid" << std::endl;
         return false;
     }
     if(Tval2 < 1 || Tval2 > 12){
+        std::cout << "month invalid" << std::endl;
         return false;
     }
     switch(Tval2){
@@ -45,6 +49,7 @@ bool SensDataEntry::checkTemporalInfo(int Tval1, int Tval2, int Tval3){
         case 10:
         case 12:
             if(Tval3 > 31){
+                std::cout << "day invalid" << std::endl;
                 return false;
             }
         case 4:
@@ -52,12 +57,15 @@ bool SensDataEntry::checkTemporalInfo(int Tval1, int Tval2, int Tval3){
         case 9:
         case 11:
             if(Tval3 > 30){
+                std::cout << "day invalid" << std::endl;
                 return false;
             }
         case 2:
             if(Tval1 % 4 == 0 && Tval3 > 29){
+                std::cout << "day invalid" << std::endl;
                 return false;
             } else if (Tval3 > 28){
+                std::cout << "day invalid" << std::endl;
                 return false;
             }
     }
@@ -70,22 +78,25 @@ bool SensDataEntry::setRandomTemporalInfo(){
 
 bool SensDataEntry::checkReadingInfo(float Rval){
     if(trunc(fabs(Rval*100))/100 < 0.0 && trunc(fabs(Rval*100))/100 > 100.0){
+        std::cout << "Reading not in bounds" << std::endl;
         return false;
     }
     return true;
 }
 
 bool SensDataEntry::checkAndSetReadingInfo(float Rval){
-    if(this->checkReadingInfo(Rval)){
+    if(this->checkReadingInfo(Rval) && !reading_init){
         this->reading_info = Rval;
         this->reading_init = true;
         return true;
+    } if(reading_init){
+        std::cout << "reading set, must reset first" << std::endl;
     }
     return false;
 }
 
 bool SensDataEntry::setRandomReadingInfo(){
-    return false;
+    return checkAndSetReadingInfo(RandomValInBounds(0.0,100.0));
 }
 
 bool SensDataEntry::checkAndSetSensorID(int IDval){
@@ -96,7 +107,10 @@ bool SensDataEntry::checkAndSetSensorID(int IDval){
 }
 
 void SensDataEntry::reset(){
-
+    temporal_info.fill(std::nan("-1"));
+    reading_info = std::nanf("1");
+    reading_init = false;
+    temporal_init = false;
 }
 
 // Getters
@@ -115,6 +129,7 @@ bool SensDataEntry::getSDE(int& Tval1, int& Tval2, int& Tval3, float& Rval, int&
         IDval = sensor_id;
         return true;
     }
+    std::cout << "not all data set" << std::endl;
     return false;
 }
 
